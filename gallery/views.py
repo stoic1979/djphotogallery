40a4cbd52522a05fbde0a12d11548406d0bd6be7
Navjot Singh <weavebytes@gmail.com>
2015-10-02 03:05:48 +0530

    url conf, view handler for home page

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
-# Create your views here.
+def home(request):
+    """
+    home page view for the website
+    """
+    return render_to_response('index.html')
