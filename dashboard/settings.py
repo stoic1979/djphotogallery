f838f9894645be0ccac0a06036c0300cb6e5ba7b
Navjot Singh <weavebytes@gmail.com>
2015-10-02 02:00:57 +0530

    updated settings

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djphotogallery',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
 }
 
 
