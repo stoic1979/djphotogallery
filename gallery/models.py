cf8600e6c8d3bbabbfab7a3859a6d0d9b9ecbc99
Navjot Singh <weavebytes@gmail.com>
2015-10-02 02:34:18 +0530

    added gallery model (draft)

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Gallery(models.Model):
+    date_added  = models.DateTimeField(default=now)
+    title       = models.CharField(max_length=64, unique=True)
+    slug        = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    is_public   = models.BooleanField(default=True)
