b50226fbbb10551fe04911a60e1b0798f44775d5
Navjot Singh <weavebytes@gmail.com>
2015-10-02 23:54:32 +0530

    added Photo model

--- gallery/models.py
+++ gallery/models.py
@@ -13,3 +13,17 @@ class Gallery(models.Model):
         function returns unicode representation of a gallery
         """
         return self.title
+
+class Photo(models.Model):
+    gallery     = models.ForeignKey(Gallery)
+    date_added  = models.DateTimeField(default=datetime.now)
+    title       = models.CharField(max_length=64)
+    slug        = models.SlugField(unique=True)
+    caption     = models.TextField(blank=True)
+    is_public   = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        """
+        function returns unicode representation of a photo
+        """
+        return self.title
