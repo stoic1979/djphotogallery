9dd98cf2d6aa4f9687d85249f64c302a4f929a98
Navjot Singh <weavebytes@gmail.com>
2016-07-12 19:09:06 +0530

    media url etc in setts

--- gallery/settings.py
+++ gallery/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'gallery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['album/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+###################################
+#                                 #
+#   PATHS FOR FILE UPLOADS etc    #
+#                                 #
+###################################
+MEDIA_URL = '/photos/'
+MEDIA_ROOT = '/home/wb9/work/stoic/djphotogallery'
