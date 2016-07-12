49df12a1c4d777ec5d78e348899118eb0234c508
Navjot Singh <weavebytes@gmail.com>
2016-07-12 18:52:37 +0530

    adding models for album

--- album/models.py
+++ album/models.py
@@ -1,5 +1,34 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Album(models.Model):
+    """
+    model for user albums
+    """
+    user = models.ForeignKey(User)	
+    title = models.CharField(max_length=128, verbose_name=_('Title'))
+    description = models.CharField(max_length=1024, verbose_name=_('Title'))
+
+    # preview/thumbnail image for an album
+    img = models.FileField(upload_to = 'photos/albums/')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
+
+
+class Photo(models.Model):
+    """
+    model for user photos
+    """
+    user = models.ForeignKey(User)	
+    album = models.ForeignKey(Album)	
+    img = models.FileField(upload_to = 'photos/')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
+    
+    def __unicode__(self):
+        """
+        function returns unicode representation of photo
+        """
+        return "%s" % self.img
