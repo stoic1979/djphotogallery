255c4bf1a85b39f8d33b5bed1a4f01a2e587ba32
Navjot Singh <weavebytes@gmail.com>
2016-07-12 18:56:06 +0530

    adding registration app

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'album.apps.AlbumConfig',
+    'reg.apps.RegConfig',	
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
